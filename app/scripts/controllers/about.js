591464d0a3289b560fe36f60d846ea0fa7b5f7bc
Andrew Hill <mdude2314@gmail.com>
2016-09-01 20:59:11 -0400

    init commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cardinalEsportsClubApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the cardinalEsportsClubApp
+ */
+angular.module('cardinalEsportsClubApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cardinalEsportsClubApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cardinalEsportsClubApp
+ */
+angular.module('cardinalEsportsClubApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
